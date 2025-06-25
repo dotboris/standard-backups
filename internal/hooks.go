9ba56e626b0882f269d9631a0f20c9fe739d3db8
Boris Bera <beraboris@gmail.com>
2025-06-25 14:44:59 -0400

    chore(hooks): add tooling to run hooks

---
+++ internal/hooks.go
@@ -0,0 +1,35 @@
+package internal
+
+import (
+	"errors"
+	"os"
+	"os/exec"
+
+	"github.com/dotboris/standard-backups/internal/config"
+)
+
+var (
+	errUnsupportedShell = errors.New("unsupported shell")
+)
+
+func runHook(hook config.HookV1) error {
+	var (
+		command string
+		args    []string
+	)
+	switch hook.Shell {
+	case "sh":
+		command = "sh"
+		args = []string{"-c", hook.Command}
+	case "bash":
+		command = "bash"
+		args = []string{"-c", hook.Command}
+	default:
+		return errUnsupportedShell
+	}
+
+	cmd := exec.Command(command, args...)
+	cmd.Stdout = os.Stderr
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
